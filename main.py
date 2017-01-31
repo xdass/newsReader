8b7f8cfffeae378a8643eabf208075b48b327c34
xdass <420600@gmail.com>
2017-01-31 16:22:51 +0500

    Реализация с использованием классов

--- main.py
+++ main.py
@@ -3,7 +3,60 @@ import re
 import html
 import time
 
-start = time.time()
+
+#https://lenta.ru/news/2017/01/19/avalanche30/
+#https://www.gazeta.ru/politics/2017/01/19_a_10481981.shtml#page1
+#http://www.rbc.ru/politics/19/01/2017/58806cec9a7947114330fbbf
+
+#query_clear_link = re.compile(r'(?<=href=\")http[s]?://+(.*?)(?=\")\".+?>(.*?)</a>')
+#query_for_links = re.compile(r'<a\b.*?(?:href=\"(.*?)\")[^>]*>(.*?)</a>')#(<a\b[^>]*>(.*?)</a>)
+
+
+class NewsParser:
+
+    def __init__(self, link):
+        self._link = link
+
+    def receive_news(self):
+        request = urllib.request.Request(self._link)
+        request.add_header('User-Agent', 'Mozilla/5.0')
+        response = urllib.request.urlopen(request)
+        page_charset = response.headers.get_content_charset()
+        raw_data = response.read().decode(page_charset)
+        return raw_data
+
+    def prepare_data(self, raw_data):
+        article_title = re.search(r'<title>(.*?)</title>', raw_data, re.MULTILINE | re.VERBOSE).group(1)
+        article_text = re.findall(r'<p>(.*?)</p>', raw_data, re.MULTILINE | re.VERBOSE)
+        clear_article = [html.unescape(item) for item in article_text]
+        clear_article = highlight_links(clear_article)
+
+        return article_title, clear_article
+
+    def generate_text(self, clear_text):
+        article = []
+        for i in clear_text:
+            r = re.search(r'<strong.*>(.*?)</strong>', i)
+            if r:
+                article.append({'title': r.group(1)})
+            else:
+                article.append({'text': i})
+        return article
+
+    def save_to_file(self, title, article):
+        with open('text.txt', 'w', encoding='utf-8') as file:
+            file.write(title)
+            for item in article:
+                if 'title' in item:
+                    file.write(text_wrap(item['title'], 80))
+                    file.write('\n\n')
+                else:
+                    file.write(text_wrap(item['text'], 80))
+                    file.write('\n\n')
+
+    def get_path_from_link(self):
+        pass
+
 
 class FootnoteNumbers:
     def __init__(self, links):
@@ -20,6 +73,7 @@ class FootnoteNumbers:
 
 def highlight_links(text):
     data = []
+    query_for_links = re.compile(r'<a\b.*?(?:href=\"(.*?)\")[^>]*>(.*?)</a>')
 
     for item in text:
         links = re.findall(query_for_links, item)
@@ -58,49 +112,3 @@ def text_wrap(text, width):
             newline = True
 
     return ''.join(s)
-
-#https://lenta.ru/news/2017/01/19/avalanche30/
-#https://www.gazeta.ru/politics/2017/01/19_a_10481981.shtml#page1
-#http://www.rbc.ru/politics/19/01/2017/58806cec9a7947114330fbbf
-
-
-newsUrl = 'http://www.rbc.ru/politics/19/01/2017/58806cec9a7947114330fbbf'
-request = urllib.request.Request(newsUrl)
-request.add_header('User-Agent', 'Mozilla/5.0')
-
-response = urllib.request.urlopen(request)
-page_charset = response.headers.get_content_charset()
-data = response.read().decode(page_charset)
-
-title = re.search(r'<title>(.*?)</title>', data, re.MULTILINE | re.VERBOSE).group(1)
-raw_text = re.findall(r'<p>(.*?)</p>', data, re.MULTILINE | re.VERBOSE)
-clear_text = [html.unescape(item) for item in raw_text]
-
-query_clear_link = re.compile(r'(?<=href=\")http[s]?://+(.*?)(?=\")\".+?>(.*?)</a>')
-#query = re.compile(r'(http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+)\">(.*?)</a>')
-query_for_links = re.compile(r'<a\b.*?(?:href=\"(.*?)\")[^>]*>(.*?)</a>')#(<a\b[^>]*>(.*?)</a>)
-
-clear_text = highlight_links(clear_text)
-
-#ToDo перенести в ф-цию
-#Разделение на заголовки и содержание(параграфы)
-article = []
-for i in clear_text:
-    r = re.search(r'<strong.*>(.*?)</strong>', i)
-    if r:
-        article.append({'title': r.group(1)})
-    else:
-        article.append({'text': i})
-
-print(time.time() - start)
-
-print(article)
-
-with open('text.txt', 'w', encoding='utf-8') as file:
-    for item in article:
-        if 'title' in item:
-            file.write(text_wrap(item['title'], 80))
-            file.write('\n\n')
-        else:
-            file.write(text_wrap(item['text'], 80))
-            file.write('\n\n')
